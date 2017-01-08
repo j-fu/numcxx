cfc62252b97a2bb3ed96412337a27cf56f236e15
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2017-01-08 23:23:27 +0100

    some comments in fem2d.hxx

--- examples/part5/fem2d.hxx
+++ examples/part5/fem2d.hxx
@@ -5,46 +5,56 @@
 #include <numcxx/tsparsematrix.hxx>
 #include <cmath>
 #include <iostream>
+
 namespace fem2d
 {
+  // Assemble stiffness matrix for mixed Dirichlet/Homogeneus Neumann
+  // heat conduction
   inline std::shared_ptr<numcxx::TSparseMatrix<double>> 
   assemble_heat_matrix(
-    numcxx::SimpleGrid &g,
-    numcxx::IArray1 &bcdirichlet
+    numcxx::SimpleGrid &g,       // Discretization grid
+    numcxx::IArray1 &bcdirichlet // Boundary regions with Dirichlet BC
     )
   {
     auto ndim=g.spacedim();
-    auto points=g.get_points();
-    auto cells=g.get_cells();
+
+    auto points=g.get_points(); // Array of global nodes
+    auto cells=g.get_cells();   // Local-global dof map
+
     int npoints=g.npoints();
     int ncells=g.ncells();
 
-    
-    auto pSLocal=numcxx::DMatrix::create(ndim+1, ndim+1);
+
+    // Global stiffness matrix
     auto pSGlobal=numcxx::DSparseMatrix::create(npoints, npoints);
-    auto pV=numcxx::DMatrix::create(ndim, ndim);
-
-    auto &SLocal=*pSLocal;
-    auto &V=*pV;
     auto &SGlobal=*pSGlobal;
-//    std::cout << points << std::endl;
-//    std::cout << cells << std::endl;
 
+    // Local stiffness matrix
+    auto pSLocal=numcxx::DMatrix::create(ndim+1, ndim+1);
+    auto &SLocal=*pSLocal;
+
+    // Local matrix of coordinate differences
+    auto pV=numcxx::DMatrix::create(ndim, ndim);
+    auto &V=*pV;
+
+    // Loop over all elements (cells) of the triangulation
     for (int icell=0; icell<ncells; icell++)
     {
       
+      // Fill matrix V
       V(0,0)= points(cells(icell,1),0)- points(cells(icell,0),0);
       V(0,1)= points(cells(icell,2),0)- points(cells(icell,0),0);
 
       V(1,0)= points(cells(icell,1),1)- points(cells(icell,0),1);
       V(1,1)= points(cells(icell,2),1)- points(cells(icell,0),1);
 
-//      std::cout << V << std::endl;
-      
+
+      // Compute determinant
       double det=V(0,0)*V(1,1) - V(0,1)*V(1,0);
       double invdet = 1.0/det;
 
       
+      // Compute entris of local stiffness matrix
       SLocal(0,0)= invdet * (  ( V(1,0)-V(1,1) )*( V(1,0)-V(1,1) )+( V(0,1)-V(0,0) )*( V(0,1)-V(0,0) ) );
       SLocal(0,1)= invdet * (  ( V(1,0)-V(1,1) )* V(1,1)          - ( V(0,1)-V(0,0) )*V(0,1) );
       SLocal(0,2)= invdet * ( -( V(1,0)-V(1,1) )* V(1,0)          + ( V(0,1)-V(0,0) )*V(0,0) );
@@ -58,47 +68,56 @@ namespace fem2d
       SLocal(2,0)=SLocal(0,2);
       SLocal(2,1)=SLocal(1,2);
 
-//      std::cout << SLocal << std::endl;
+      // Assemble into global stiffness matrix
       for (int i=0;i<=ndim;i++)
         for (int j=0;j<=ndim;j++)
           SGlobal(cells(icell,i),cells(icell,j))+=SLocal(i,j);
     }    
 
 
+    // Assemble boundary conditions (Dirichlet penalty method)
     int nbfaces=g.nbfaces();
     auto bfaces=g.get_bfaces();
     auto bfaceregions=g.get_bfaceregions();
 
     for (int ibface=0; ibface<nbfaces; ibface++)
     {
+      // Obtain number of boundary condition
       int ireg=bfaceregions(ibface);
+
+      // Check if it is "Dirichlet"
       if (bcdirichlet(ireg))
       {
+        // Assemble penalty values
         int i;
         i=bfaces(ibface,0);
         SGlobal(i,i)+=1.0e30;
+
         i=bfaces(ibface,1);
         SGlobal(i,i)+=1.0e30;
       }
     }
+    // Return global stiffness matrix
     return pSGlobal;
   }
 
+  // Assemble zero right hand side for mixed Dirichlet/Homogeneus Neumann problem
   std::shared_ptr<numcxx::TArray1<double>>
   assemble_heat_rhs(
-    numcxx::SimpleGrid &g,
-    numcxx::IArray1 &bcdirichlet,
-    numcxx::DArray1 &bcval
+    numcxx::SimpleGrid &g,       // Discretization grid
+    numcxx::IArray1 &bcdirichlet,// Boundary regions with Dirichlet BC
+    numcxx::DArray1 &bcval       // Dirichlet boundary values
     )
   {
     int npoints=g.npoints();
-
-    auto pRhs=numcxx::DArray1::create(npoints);
-    auto &Rhs=*pRhs;
-    Rhs=0.0;
     int nbfaces=g.nbfaces();
     auto bfaces=g.get_bfaces();
     auto bfaceregions=g.get_bfaceregions();
+
+    auto pRhs=numcxx::DArray1::create(npoints);
+    auto &Rhs=*pRhs;
+
+    Rhs=0.0;
 
     for (int ibface=0; ibface<nbfaces; ibface++)
     {
@@ -106,8 +125,10 @@ namespace fem2d
       if (bcdirichlet(ireg))
       {
         int i;
+
         i=bfaces(ibface,0);
         Rhs(i)+=1.0e30*bcval(ireg);
+
         i=bfaces(ibface,1);
         Rhs(i)+=1.0e30*bcval(ireg);
       }
