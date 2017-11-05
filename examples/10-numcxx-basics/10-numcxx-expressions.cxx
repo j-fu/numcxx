004a6345335869fdf586f5f7b8704a49328811cc
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2017-11-06 00:10:55 +0100

    Expression template test + example

--- examples/10-numcxx-basics/10-numcxx-expressions.cxx
+++ examples/10-numcxx-basics/10-numcxx-expressions.cxx
@@ -1,58 +1,102 @@
 ///
 /// \example 10-numcxx-expressions.cxx
 ///
-/// Demonstrate the basic usage of vectors and expression templates in numcxx.
+/// Demonstrate the basic usage of arrays and expression templates in numcxx.
 /// 
-
+/// Epression templates  provide two advantages: they allow to work with
+/// vectors using the common mathematical notations. Furthermore, for
+/// complex expressions, they allow to avoid intermediate storage of results
+/// in vectors, providing a possible speed advantage.
+///
+/// One potential pitfall for expression templates is the use of auto to detect
+/// the type of result from an expression template, see e.g. https://eigen.tuxfamily.org/dox/TopicPitfalls.html
+/// If you try to do this, please use numcxx::arrayexpr to obtain the proper
+/// data type.
 #include <iostream>
-#include <iomanip>
-#include <cassert>
 #include <numcxx/numcxx.hxx>
 
 int main()
 {
     const int n=3;
-
-    /// Declare four arrays with double elements.
-    /// Like in the case with std::vector, the contents of a class
-    /// object is placed on the stack, but the large data array is 
-    /// put on the heap.
+    //////////////////////////////////////////////////////////////
+    // Declare three  arrays with double  elements.  Like in  the case
+    // with std::vector, the contents of  a class object is placed on
+    // the stack, but the large data array is put on the heap.
 
     numcxx::TArray1<double> A(n);
     numcxx::TArray1<double> B(n);
     numcxx::TArray1<double> C(n);
-    //numcxx::TArray1<double> D(n);
+    numcxx::TArray1<double> D(n);
 
-    /// Declare and initialize  a matrix
-    numcxx::TMatrix<double> Mat{{1,2,3},{4,5,6},{7,8,9}};
+    //////////////////////////////////////////////////////////////
+    // We can assign constant values to an array.
+    // The expression template trick allows to do the same for B
+    // as the loop does for A.
+    for (int i=0;i<n;i++) 
+      A(i)=3.0;
+    B=3.0;
 
-    // We can initialize the vectors with constant values.
-    A=0.0;
-    B=1.0;
-    C=2.0;
-    A=B+3.1;
-    C=3.1+B;
-    assert(numcxx::norm2(A-C)==0);
-    A(1)=19;
-
-    auto D = numcxx::arrayexpr(A*3);
-//    std::cout << D;
-    //D =A*3.0;
-    std::cout << typeid(decltype(D)).name() << std::endl;
-    std::cout << D[1] << std::endl;
-    A=D*2.0;
-//    A=3*(B+C/3.0)-(2.0*D);
-    std::cout << std::endl<< std::endl;
-    // Two ways to perform matrix-vector multiplication
-    // Via expression template
-    B=Mat*A;
-    // Via apply method (calling dgemm of lapack)
-    Mat.apply(A,C);
-
+    std::cout << "Assigning constant values to an array:" << std::endl;
+    // numcxx arrays can be printed via iostreams.
+    std::cout << "A:"<< std::endl << A << std::endl;
+    std::cout << "B:"<< std::endl << B << std::endl;
     
-    std::cout<< B << std::endl;
-    std::cout<< C << std::endl;
+    //////////////////////////////////////////////////////////////
+    for (int i=0;i<n;i++) 
+      A(i)=i+5;
+    B=A;
 
-    auto diffnorm=numcxx::norm2(B-C);
-    std::cout << "|B-C|="<<std::setprecision(10) <<  diffnorm << std::endl;
+    std::cout << "Copying data from one array to the other:" << std::endl;
+    std::cout << "A:"<< std::endl << A << std::endl;
+    std::cout << "B:"<< std::endl << B << std::endl;
+
+    //////////////////////////////////////////////////////////////
+    // On arrays of the same size, we can perform the operations
+    // allowed for elements of a vector space, i.e. addition, subraction and
+    // multiplication by scalars.
+    // The expression template trick allows to do the same for C
+    // as the loop does for D.
+    for (int i=0;i<n;i++)
+      C(i)=3*A(i)+B(i);
+    D=3*A+B;
+    std::cout << "Arithmetic expressions:" << std::endl;
+    std::cout << "C:"<< std::endl << C << std::endl;
+    std::cout << "D:"<< std::endl << C << std::endl;
+
+   
+    //////////////////////////////////////////////////////////////
+    // In array expressions, stay safe and avoid 
+    // auto on the left hand side!
+    numcxx::TArray1<double> E=A+17*B;
+
+    // If you want to use auto, do it like this.
+    auto F=numcxx::arrayexpr(A+17*B);
+
+    // The alternative
+    // auto F=A+17*B;
+    // won't work with << or later use of F on the left
+    // hand side of expressions because the type of F detected
+    // by auto is the expression template, not the vector.
+    // Here, it would  be detected at compile time.
+    
+    std::cout << "Avoiding auto with expression templates:" << std::endl;
+    std::cout << "E:"<< std::endl << E << std::endl;
+    std::cout << "F:"<< std::endl << F << std::endl;
+    
+    //////////////////////////////////////////////////////////////
+    // This is the real dangerous situation with auto, 
+    // described in https://eigen.tuxfamily.org/dox/TopicPitfalls.html
+    auto G=numcxx::arrayexpr(19*A+10*B);
+    
+    // With
+    // auto G=19*A+10*B;
+    // G would be an instance of an intermediate type which contains
+    // A and B and the possibility to calculate the result of the rhs expression.
+    // Changing A would change also A in G and lead to a different result.
+    // This is not detected at compile time!
+    A(1)++;
+    E=G+E;
+    std::cout << "Pitfalls of auto:" << std::endl;
+    std::cout << "E:"<< std::endl << E << std::endl;
+    
 }
--- tests/test-expressions.cxx
+++ tests/test-expressions.cxx
@@ -1,32 +1,173 @@
 #include <iostream>
 #include <numcxx/numcxx.hxx>
+#include <stdexcept> 
 
-/// This is a test for the expression templates implemented
-/// in numcxx. 
+//
+// Check all basic operations for expression templates
+//
+
+template <typename T1, typename T2, typename T3>
+void check(T1& C1, T2& C2, T3& C3)
+{
+  int n=C1.size();
+  std::cout << typeid(T1).name() << std::endl;
+  std::cout << typeid(T2).name() << std::endl;
+  std::cout << typeid(T3).name() << std::endl;
+  
+  for (int i=0;i<n;i++)
+  {
+    //std::cout << i << " " << C1[i] <<" " << C2[i] <<" " << C3[i] << std::endl;
+    if (C1[i]!=C2[i]) 
+    {
+      std::cout <<"error in expression template" << std::endl;
+      exit(1);
+    }
+    if (C1[i]!=C3[i]) 
+    {
+      std::cout <<"error in expression template" << std::endl;
+      exit(1);
+    }
+  }
+  std::cout << std::endl;
+}
+
 int main()
 {
-    const int n=3;
+    const int n=100;
+    
     numcxx::DArray1 A(n);
     numcxx::DArray1 B(n);
-    numcxx::DArray1 C(n);
-    numcxx::DArray1 D(n);
-
-    numcxx::TMatrix<double> Mat{{1,2,3},{4,5,6},{7,8,9}};
-    A=0.0;
-    B=1.0;
-    C=2.0;
-    D=3.25;
-    A=B+3.1;
-    A=3.1+B;
-    D=A*3.0;
-    A=3*(B+C/3.0)-4.0*D/2.0;
+    numcxx::DArray1 C1(n);
+    for (int i=0;i<n;i++)
+    {
+      A(i)=10+i;
+      B(i)=10+n-i-1;
+    }
     
-    // Two ways to perform matrix-vector multiplication
-    // Via expression template
-    B=Mat*A;
-    // Via apply method (calling dgemm of lapack)
-    Mat.apply(A,C);
+    
+    {
+      std::cout << "operation A+B" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=A(i)+B(i);
+      numcxx::DArray1 C2=A+B;
+      auto C3=A+B;
+      check(C1,C2,C3);
+    }
 
-    std::cout<< B << std::endl;
-    std::cout<< C << std::endl;
+    {
+      std::cout << "operation A-B" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=A(i)-B(i);
+      numcxx::DArray1 C2=A-B;
+      auto C3=A-B;
+      check(C1,C2,C3);
+    }
+
+    {
+      double a=3.0;
+      std::cout << "operation a*B" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=a*B(i);
+      numcxx::DArray1 C2=a*B;
+      auto C3=3.0*B;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+    {
+      double b=3.0;
+      std::cout << "operation A*b" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=A(i)*b;
+      numcxx::DArray1 C2=A*b;
+      auto C3=A*b;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+
+    {
+      double a=3.0;
+      std::cout << "operation a+B" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=a+B(i);
+      numcxx::DArray1 C2=a+B;
+      auto C3=3.0+B;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+    {
+      double b=3.0;
+      std::cout << "operation A+b" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=A(i)+b;
+      numcxx::DArray1 C2=A+b;
+      auto C3=A+b;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+    {
+      double a=3.0;
+      std::cout << "operation a-B" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=a-B(i);
+      numcxx::DArray1 C2=a-B;
+      auto C3=3.0-B;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+    {
+      double b=3.0;
+      std::cout << "operation A-b" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=A(i)-b;
+      numcxx::DArray1 C2=A-b;
+      auto C3=A-b;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+    {
+      double b=3.0;
+      std::cout << "operation A/b" << std::endl;
+      for (int i=0;i<n;i++) 
+        C1(i)=A(i)/b;
+      numcxx::DArray1 C2=A/b;
+      auto C3=A/b;
+      check(C1,C2,C3);
+      C1=C3*2.0; // this triggers optimizing away scalar reference in g++
+    }
+
+
+    {
+      std::cout << "operation M*A" << std::endl;
+      numcxx::DMatrix M(n,n);
+      for (int i=0;i<n;i++)
+        for (int j=0;j<n;j++)
+          M(i,j)=10*i+3*j+1;
+      M.apply(A,C1);
+      numcxx::DArray1 C2=M*A;
+      auto C3=M*A;
+      check(C1,C2,C3);
+    }
+
+    {
+      
+      std::cout << "operation M*A (sparse)" << std::endl;
+      numcxx::DSparseMatrix M(n,n);
+      for (int i=0;i<n;i++)
+        for (int j=0;j<n;j++)
+          M(i,j)=10*i+3*j+1;
+      M.flush();
+      M.apply(A,C1);
+      numcxx::DArray1 C2=M*A;
+      auto C3=M*A;
+      check(C1,C2,C3);
+    }
+
+
 }
+
