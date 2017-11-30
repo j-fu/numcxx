be0eb808ffdfe84d62760e40f51d5f843ecff6f3
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2017-11-30 08:55:51 +0100

    CG example

---
+++ examples/10-numcxx-basics/17-numcxx-cg.cxx
@@ -0,0 +1,49 @@
+/// 
+///  \example 17-numcxx-cg.ccxx
+///
+/// Use of cg for sparse matrices with shared ptr idiom
+/// 
+
+#include <iostream>
+#include <limits>
+#include <cassert>
+#include <numcxx/numcxx.hxx>
+#include "netlib/netlib.hxx"
+
+
+int main(void)
+{
+    int n=5000;
+    int max_iter=100;
+    double tol=1.0e-14;
+
+    auto pA=numcxx::DSparseMatrix::create(n,n);
+    auto pF=numcxx::DArray1::create(n);
+    auto pU=numcxx::DArray1::create(n);
+    
+    auto &A=*pA;
+    auto &F=*pF;
+    auto &U=*pU;
+    
+    F=1.0;
+    for (int i=0;i<n;i++)
+    {
+        A(i,i)=3.0;
+        if (i>0) A(i,i-1)=-1;
+        if (i<n-1) A(i,i+1)=-1;
+    }
+    pA->flush();
+    
+
+    auto pJacobi=std::make_shared<numcxx::TPreconJacobi<double>>(pA);
+
+ 
+    U=0.0;
+    netlib::CG(A,U,F,*pJacobi,max_iter,tol);
+    double residual=normi(A*U-F);
+    std::cout << "residual:" << residual << std::endl;
+
+}
+
+
+
